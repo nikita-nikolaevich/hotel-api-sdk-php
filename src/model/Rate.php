0ff41945abaf3f70fb82fb235f47238d32c425c5
Vasilis Mavromatis <vmavromatis@hotelbeds.com>
2017-11-13 16:50:02 +0100

    Update Rate.php

--- src/model/Rate.php
+++ src/model/Rate.php
@@ -37,7 +37,6 @@ namespace hotelbeds\hotel_api_sdk\model;
  * @property array taxes List of taxes
  * @property array promotions List of promotions
  * @property array dailyRates List of daily rates
- * @property integer allotment Number of rooms available for a particular room type
  */
 
 class Rate extends ApiModel
@@ -72,8 +71,7 @@ class Rate extends ApiModel
              "cancellationPolicies" => "array",
              "taxes" => "array",
              "promotions" => "array",
-             "dailyRates" => "array",
-             "allotment" => "integer"];
+             "dailyRates" => "array"];
 
         if ($data !== null)
         {
